68b88c8e6e620457d46980895d9711f10eb51186
Williham Totland <williham.totland@gmail.com>
2020-03-02 11:50:35 +0100

    Adds failure descriptions to tests.

--- Tests/BasementDriverTests/PasswordTests.swift
+++ Tests/BasementDriverTests/PasswordTests.swift
@@ -6,22 +6,22 @@ final class PasswordTests: XCTestCase {
   func testPassword() {
     let password = Password(hashing: "matching password")
 
-    XCTAssertTrue(password.validate(against: "matching password"))
-    XCTAssertFalse(password.validate(against: "non-matching password"))
+    XCTAssertTrue(password.validate(against: "matching password"), "Unable to validate a matching password")
+    XCTAssertFalse(password.validate(against: "non-matching password"), "Validated a non-matching password")
   }
 
   func testRepassword() {
     let password = Password(Password(hashing: "matching password").description)
 
-    XCTAssertTrue(password.validate(against: "matching password"))
-    XCTAssertFalse(password.validate(against: "non-matching password"))
+    XCTAssertTrue(password.validate(against: "matching password"), "Unable to validate a matching password")
+    XCTAssertFalse(password.validate(against: "non-matching password"), "Validated a non-matching password")
   }
 
   func testSalt() {
     let passwordOne = Password(hashing: "matching password")
     let passwordTwo = Password(hashing: "matching password")
 
-    XCTAssertNotEqual(passwordOne, passwordTwo)
+    XCTAssertNotEqual(passwordOne, passwordTwo, "Matching passwords with different salts match")
   }
 
   func testResalt() {
@@ -29,8 +29,8 @@ final class PasswordTests: XCTestCase {
     let passwordTwo = Password(hashing: "matching password", salt: "salt")
     let passwordThree = Password(hashing: "matching password", salt: "pepper")
 
-    XCTAssertEqual(passwordOne, passwordTwo)
-    XCTAssertNotEqual(passwordTwo, passwordThree)
+    XCTAssertEqual(passwordOne, passwordTwo, "Matching passwords with the same salt don't match")
+    XCTAssertNotEqual(passwordTwo, passwordThree, "Matching passwords with different salts match")
   }
 
 }

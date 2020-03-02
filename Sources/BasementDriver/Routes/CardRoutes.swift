190ecf8d2ecce0177691b592fb9088a8c9403a68
Williham Totland <williham.totland@gmail.com>
2020-03-02 08:49:20 +0100

    Adds handler stubs.

--- Sources/BasementDriver/Routes/CardRoutes.swift
+++ Sources/BasementDriver/Routes/CardRoutes.swift
@@ -5,7 +5,48 @@ import PerfectHTTP
 extension BasementDriver {
 
   static var cardRoutes: Routes {
-    Routes()
+    var cardRoutes = Routes(baseUri: "/cards")
+
+    cardRoutes.add(method: .post, uri: "/create", handler: createHandler)
+    cardRoutes.add(method: .get, uri: "/list", handler: listHandler)
+
+    var fetchedRoutes = Routes(baseUri: "/{card_id}", handler: fetchHandler)
+
+    fetchedRoutes.add(method: .get, uri: "/read", handler: readHandler)
+    fetchedRoutes.add(method: .post, uri: "/update", handler: updateHandler)
+    fetchedRoutes.add(method: .get, uri: "/delete", handler: deleteHandler)
+
+    cardRoutes.add(fetchedRoutes)
+
+    return cardRoutes
+  }
+
+}
+
+private extension BasementDriver {
+
+  static func createHandler(request: HTTPRequest, response: HTTPResponse) {
+
+  }
+
+  static func listHandler(request: HTTPRequest, response: HTTPResponse) {
+
+  }
+
+  static func fetchHandler(request: HTTPRequest, response: HTTPResponse) {
+
+  }
+
+  static func readHandler(request: HTTPRequest, response: HTTPResponse) {
+
+  }
+
+  static func updateHandler(request: HTTPRequest, response: HTTPResponse) {
+
+  }
+
+  static func deleteHandler(request: HTTPRequest, response: HTTPResponse) {
+
   }
 
 }
--- Sources/BasementDriver/Routes/UserRoutes.swift
+++ Sources/BasementDriver/Routes/UserRoutes.swift
@@ -6,7 +6,44 @@ import PerfectSession
 extension BasementDriver {
 
   static var userRoutes: Routes {
-    Routes()
+    var userRoutes = Routes(baseUri: "/user", handler: baseHandler)
+
+    userRoutes.add(method: .post, uri: "/register", handler: registerHandler)
+    userRoutes.add(method: .post, uri: "/login", handler: loginHandler)
+
+    userRoutes.add(method: .get, uri: "/profile", handler: profileHandler)
+    userRoutes.add(method: .get, uri: "/cards", handler: cardsHandler)
+
+    return userRoutes
+  }
+
+}
+
+private extension BasementDriver {
+
+  static func baseHandler(request: HTTPRequest, response: HTTPResponse) {
+    if let username = request.session?.userid, let user = try? User.named(username) {
+      request.scratchPad["user"] = user
+    }
+
+    response
+      .next()
+  }
+
+  static func registerHandler(request: HTTPRequest, response: HTTPResponse) {
+
+  }
+
+  static func loginHandler(request: HTTPRequest, response: HTTPResponse) {
+
+  }
+
+  static func profileHandler(request: HTTPRequest, response: HTTPResponse) {
+
+  }
+
+  static func cardsHandler(request: HTTPRequest, response: HTTPResponse) {
+
   }
 
 }

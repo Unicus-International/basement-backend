68ff91a0c3e80111a1d5b1c366df07c3f5d3665c
Williham Totland <williham.totland@gmail.com>
2020-03-03 10:46:12 +0100

    Adds debug logging to user routes.

--- Sources/BasementDriver/Routes/UserRoutes.swift
+++ Sources/BasementDriver/Routes/UserRoutes.swift
@@ -25,6 +25,7 @@ private extension BasementDriver {
     guard
       (request.session?.userid ?? "") == ""
     else {
+      log.debug(message: "403 User already logged in")
       return response
         .completed(status: .forbidden)
     }
@@ -37,6 +38,7 @@ private extension BasementDriver {
       let passwordTwo = request.param(name: "password_two"),
       passwordOne == passwordTwo
     else {
+      log.debug(message: "400 Missing or malformed field")
       return response
         .completed(status: .badRequest)
     }
@@ -44,6 +46,7 @@ private extension BasementDriver {
     guard
       try! User.isRegisterable(username: username)
     else {
+      log.debug(message: "403 Username \(username) taken or invalid")
       return response
         .completed(status: .forbidden)
     }
@@ -51,10 +54,12 @@ private extension BasementDriver {
     guard
       let user = try? User.newUser(username: username, password: passwordOne, emailAddress: emailAddress)
     else {
+      log.debug(message: "500 Could not create new user")
       return response
         .completed(status: .internalServerError)
     }
 
+    log.info(message: "201 Created")
     request.session?.userid = user.username
     response
       .addHeader(.location, value: "profile")
@@ -66,6 +71,7 @@ private extension BasementDriver {
       let username = request.param(name: "username"),
       let password = request.param(name: "password")
     else {
+      log.debug(message: "400 Missing field")
       return response
         .completed(status: .badRequest)
     }
@@ -74,10 +80,12 @@ private extension BasementDriver {
       let user = try? User.named(username),
       user.checkPassword(password)
     else {
+      log.debug(message: "403 No such user or wrong password")
       return response
         .completed(status: .forbidden)
     }
 
+    log.info(message: "204 No Content")
     request.session?.userid = user.username
     response
       .completed(status: .noContent)
@@ -87,10 +95,12 @@ private extension BasementDriver {
     guard
       let user = request.scratchPad["user"] as? User
     else {
+      log.debug(message: "403 No user logged in")
       return response
         .completed(status: .forbidden)
     }
 
+    log.info(message: "200 OK")
     response
       .JSON(encoding: user)
   }
@@ -99,10 +109,12 @@ private extension BasementDriver {
     guard
       let user = request.scratchPad["user"] as? User
     else {
+      log.debug(message: "403 No user logged in")
       return response
         .completed(status: .forbidden)
     }
 
+    log.info(message: "200 OK")
     response
       .JSON(encoding: user.cards)
   }
